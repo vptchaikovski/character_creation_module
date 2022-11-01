75d3d0ea32105ea377154c7113b1046b68ab78ce
Vladimir Tchaykovskiy <tchaikovski@mail.ru>
2022-11-01 22:21:55 +0300

    Добавили докстринги для функций

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from random import randint
+from graphic_arts.start_game_banner import run_screensaver
+
+
+from random import randint
 
 
 def attack(char_name, char_class):
+    """Attack hero."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(3, 5)}')
@@ -14,6 +19,7 @@ def attack(char_name, char_class):
 
 
 def defence(char_name, char_class):
+    """Defence hero."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} ед. урона')
     if char_class == 'mage':
@@ -23,6 +29,7 @@ def defence(char_name, char_class):
 
 
 def special(char_name, char_class):
+    """Attack special hero."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -33,6 +40,7 @@ def special(char_name, char_class):
 
 
 def start_training(char_name, char_class):
+    """Start training hero."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — великий мастер ближнего боя.')
     if char_class == 'mage':
@@ -57,6 +65,7 @@ def start_training(char_name, char_class):
 
 
 def choice_char_class():
+    """Choice hero and class."""
     approve_choice = None
     char_class = None
     while approve_choice != 'y':
@@ -78,13 +87,14 @@ def choice_char_class():
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
-    char_name = input('...назови себя: ')
+    char_name: str = input('...назови себя: ')
     print(f'Здравствуй, {char_name}! '
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
-    char_class = choice_char_class()
+    char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
